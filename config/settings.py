10b890257a19335b9437d7d0a8eb969cfe2f34b4
Pro Reload <rehousing.com@gmail.com>
2026-07-01 12:52:32 +0530

    Add EMAIL_TIMEOUT=8 so SMTP fails fast and surfaces the real error (avoid request timeout)

--- config/settings.py
+++ config/settings.py
@@ -200,4 +200,5 @@ EMAIL_PORT = env.int("EMAIL_PORT", default=25)
 EMAIL_HOST_USER = env("EMAIL_HOST_USER", default="")
 EMAIL_HOST_PASSWORD = env("EMAIL_HOST_PASSWORD", default="")
 EMAIL_USE_TLS = env.bool("EMAIL_USE_TLS", default=False)
+EMAIL_TIMEOUT = env.int("EMAIL_TIMEOUT", default=8)  # fail fast if SMTP is slow/blocked
 DEFAULT_FROM_EMAIL = env("DEFAULT_FROM_EMAIL", default="Digital Munshi <no-reply@digitalmunshi.in>")
